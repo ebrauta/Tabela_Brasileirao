3d1c71dc8328da95046615e2bbcf708f0a2a797e
Eduardo Rauta <ebrauta@gmail.com>
2020-10-07 16:35:14 -0300

    inserir cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('dotenv/config');
 const fs = require("fs");
+const cors = require("cors");
 const request = require("request");
 const cheerio = require("cheerio");
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 // Passo 1
 app.get("/", function (req, res) {
